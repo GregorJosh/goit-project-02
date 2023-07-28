93ef3ab96abcf8e74b3dbd04eacc8d95f298af2c
Samcia <samantta689@gmail.com>
2023-07-28 16:04:17 +0200

    first cmt

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,3 +1,28 @@
 import app from './global/app';
 
+const openAboutWindow = document.getElementById('about-us');
+const closeAboutWindow = document.getElementById('about-window-close');
+const backdropAboutWindow = document.getElementById('about-window-backdrop');
+
+openAboutWindow.addEventListener('click', openModal);
+closeAboutWindow.addEventListener('click', closeModal);
+backdropAboutWindow.addEventListener('keydown', closeModal);
+backdropAboutWindow.addEventListener('click', escapeModal);
+
+const closeModal = () => {
+  document.removeEventListener('keydown', escapeModal);
+  document.removeEventListener('click', closeModal);
+  backdropAboutWindow.classList.add('hidden');
+};
+
+const openModal = () => {
+  backdropAboutWindow.classList.remove('idden');
+};
+
+const escapeModal = e => {
+  if (e.key === 'Escape') {
+    closeModal();
+  }
+};
+
 app.apiTest('footer.js');
