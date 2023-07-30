0d2a359673f02e1fc978050f0d81e183e60b1e13
Samcia <samantta689@gmail.com>
2023-07-30 17:02:49 +0200

    About Window fixes

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,2 +1,28 @@
 import app from './global/app';
 
+const openModal = document.getElementById(app.ABOUT_LINK_A_ELEMENT_ID);
+const backdropModal = document.getElementById(
+  app.ABOUT_WINDOW_BACKDROP_DIV_ELEMENT_ID
+);
+const closeModal = document.getElementById('about-window-close');
+
+openModal.addEventListener('click', function (e) {
+  e.preventDefault();
+  backdropModal.style.display = 'block';
+});
+
+closeModal.addEventListener('click', function () {
+  backdropModal.style.display = 'none';
+});
+
+window.addEventListener('click', function (event) {
+  if (event.target === backdropModal) {
+    backdropModal.style.display = 'none';
+  }
+});
+
+document.addEventListener('keydown', function (ev) {
+  if (ev.key === 'Escape') {
+    backdropModal.style.display = 'none';
+  }
+});
