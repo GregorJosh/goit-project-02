ca168b367e09005ccde19ad8659e68ef4da9fc80
Samcia <samantta689@gmail.com>
2023-07-30 15:29:06 +0200

    lil fix bux stil dnt wrk

--- src/js/footer.js
+++ src/js/footer.js
@@ -5,24 +5,23 @@ const closeAboutWindow = document.getElementById('about-window-close');
 const backdropAboutWindow = document.getElementById('about-window-backdrop');
 
 openAboutWindow.addEventListener('click', openModal);
-closeAboutWindow.addEventListener('click', closeModal);
-backdropAboutWindow.addEventListener('keydown', closeModal);
-backdropAboutWindow.addEventListener('click', escapeModal);
 
-const closeModal = () => {
+function closeModal() {
   document.removeEventListener('keydown', escapeModal);
   document.removeEventListener('click', closeModal);
   backdropAboutWindow.classList.add('hidden');
-};
-
-const openModal = () => {
+}
+function openModal() {
   backdropAboutWindow.classList.remove('hidden');
-};
+  closeAboutWindow.addEventListener('click', closeModal);
+  backdropAboutWindow.addEventListener('keydown', closeModal);
+  backdropAboutWindow.addEventListener('click', escapeModal);
+}
 
-const escapeModal = e => {
+function escapeModal(e) {
   if (e.key === 'Escape') {
     closeModal();
   }
-};
+}
 
 app.apiTest('footer.js');
