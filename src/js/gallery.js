20b4d63ab38dd44069e7cd0e0200bc5c26acc4c9
Proofwolf <65850243+Proofwolf@users.noreply.github.com>
2023-07-31 11:04:35 +0200

    Genres+ID

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -5,7 +5,7 @@ app.getMoviesTodayTrends = async function (page = app.currentPage) {
     app.MOVIE_CARDS_PARENT_ELEMENT_ID
   );
 
-  const movies = await app.api.get(`movie/popular?page=${page}`);
+  const movies = await app.api.get('trending/movie/day');
   const movieCards = await app.showMovieCards(movies);
 
   galleryULElement.insertAdjacentHTML('afterbegin', movieCards);
@@ -13,13 +13,19 @@ app.getMoviesTodayTrends = async function (page = app.currentPage) {
 
 app.renderMovieCardHTML = async function (movieId) {
   const movieObject = await app.api.get('movie/' + movieId);
-  const poster = movieObject.poster_path;
-  const posterUrl = 'https://image.tmdb.org/t/p/w500';
-  const movieGenres = [];
-
+   const poster = movieObject.poster_path;
+   const posterUrl = 'https://image.tmdb.org/t/p/w500';
+  const movieGenres = []; 
   for (const genre of movieObject.genres) {
     movieGenres.push(genre.name);
   }
+  
+    if (movieGenres.length > 2) {
+      movieG= `${movieGenres[0]},${movieGenres[1]},Other`;
+    } else {
+      movieG= movieGenres;
+    } 
+  
   const movieYear = movieObject.release_date.split('-')[0];
   const title = movieObject.original_title;
   const votes = movieObject.vote_average;
@@ -27,13 +33,13 @@ app.renderMovieCardHTML = async function (movieId) {
   const about = movieObject.overview;
 
   return `
-   <li class = "movie-card">
+   <li class = "movie-card" id="app.createMovieCardId(movieId)">
        <div class="movie-thumb">
         <img class="movie-image" src="${posterUrl}${poster}" alt="Poster image" loading="lazy" />
        </div>
       <div class="movie-info">
         <p class="movie-name">${title}</p>
-        <p class="movie-genres">${movieGenres} | ${movieYear}</p>
+        <p class="movie-genres">${movieG} | ${movieYear}</p>
       </div>
     </li>`;
 };
