e4ef33eff4e420930ec9f22ea4b46a77fc4b15e8
Łukasz Paradowski <123119119+Lukaszz2003@users.noreply.github.com>
2023-08-01 07:45:30 +0200

    keyword

--- src/js/keyword.js
+++ src/js/keyword.js
@@ -2,32 +2,6 @@ import app from './global/app';
 
 app.apiTest('keyword.js');
 
-// app.getMoviesByKeyWord = async function (keyword) {
-//   app.currentKeyword = keyword;
-//   const keyword = await app.api.get('search/keyword?query=${keyword}');
-//   console.log(keyword);
-//   return keyword;
-// };
-
-// app.getMoviesByKeyWord();
-
-// app.getMoviesByKeyWord = async function (keyword) {
-//   Loading.circle();
-//   app.currentKeyword = keyword;
-//   const keywordData = await app.api.get(
-//     `search/keyword?query=${keyword}&page=${app.currentPage}`
-//   );
-//   Loading.remove();
-//   if (keywordData.total_results > 0) {
-//     return keywordData;
-//   } else {
-//     app.onSearchFailed();
-//     return {};
-//   }
-
-//   Notify.success('Notiflix Test');
-// };
-
 app.getMoviesByKeyWord = async function (keyword) {
   const galleryULElement = document.getElementById(
     app.MOVIE_CARDS_PARENT_ELEMENT_ID
@@ -48,17 +22,3 @@ app.getMoviesByKeyWord = async function (keyword) {
     app.onSearchFailed();
   }
 };
-
-//   app.currentKeyword = keyword;
-
-//   const keywordData = await app.api.get(`search/keyword?query=${keyword}&page${app.currentPage}`);
-
-//   if (keywordData.total_results > 0) {
-//     return keywordData;
-//   } else {
-//     app.onSearchFailed();
-//     throw new Error('No movies found');
-//   }
-// };
-
-// app.getMoviesByKeyWord('keyword');
